1ac3246562573e0bc78d19bb71a744c16b673d87
AFz <0xafz@proton.me>
2024-11-16 20:42:25 +0330

    chore: directly using kafka manifest in terraform file

--- prod/terraform/services/kafka/main.tf
+++ prod/terraform/services/kafka/main.tf
@@ -37,5 +37,53 @@ resource "helm_release" "strimzi" {
 }
 
 resource "kubernetes_manifest" "kafka_cluster" {
-  manifest = yamldecode(file("../../../k8s/kafka/kafka-cluster.yml"))
+  manifest = {
+    "apiVersion" = "kafka.strimzi.io/v1beta2"
+    "kind"       = "Kafka"
+    "metadata" = {
+      "name"      = "kafka-cluster"
+      "namespace" = kubernetes_namespace.kafka.metadata[0].name
+    }
+    "spec" = {
+      "kafka" = {
+        "replicas" = 3
+        "listeners" = [
+          {
+            "name" = "plain"
+            "port" = 9092
+            "type" = "internal"
+            "tls"  = false
+          },
+          {
+            "name" = "tls"
+            "port" = 9093
+            "type" = "internal"
+            "tls"  = true
+          }
+        ]
+        "storage" = {
+          "type" = "ephemeral"
+        }
+        "config" = {
+          "auto.create.topics.enable"                = "true"
+          "offsets.topic.replication.factor"         = 3
+          "transaction.state.log.replication.factor" = 3
+          "transaction.state.log.min.isr"            = 2
+          "default.replication.factor"               = 3
+          "min.insync.replicas"                      = 2
+          "log.message.format.version"               = "3.1"
+        }
+      }
+      "zookeeper" = {
+        "replicas" = 3
+        "storage" = {
+          "type" = "ephemeral"
+        }
+      }
+      "entityOperator" = {
+        "topicOperator" = {}
+        "userOperator"  = {}
+      }
+    }
+  }
 }
