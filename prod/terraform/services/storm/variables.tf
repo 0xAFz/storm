2430d97df86ec643797acd3b2415a7515f40c756
AFz <0xafz@proton.me>
2024-11-21 01:59:10 +0330

    feat: get domain from .env

--- prod/terraform/services/storm/variables.tf
+++ prod/terraform/services/storm/variables.tf
@@ -1,15 +1,20 @@
 variable "CI_REGISTRY_USER" {
-  description = "Gitlab registry username"
+  description = "gitlab registry username"
   type        = string
 }
 
 variable "CI_REGISTRY_PASSWORD" {
-  description = "Gitlab registry access token"
+  description = "gitlab registry access token"
   type = string
 }
 
 variable "CI_REGISTRY_EMAIL" {
-  description = "Gitlab registry email"
+  description = "gitlab registry email"
+  type = string
+}
+
+variable "SUBDOMAIN" {
+  description = "sub domain of project"
   type = string
 }
 
