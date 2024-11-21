eb91a149815b9605b8ae6f786fd435f335f34967
AFz <0xafz@proton.me>
2024-11-21 20:20:16 +0330

    fix: update inventory structure

--- prod/ansible/inventory/dynamic.py
+++ prod/ansible/inventory/dynamic.py
@@ -3,11 +3,12 @@ import json
 import yaml
 import subprocess
 
-terraform_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'terraform', 'cluster')
+terraform_dir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "..", "..", "terraform", "cluster"
+)
 
 terraform_output = subprocess.check_output(
-    ["terraform", "output", "-json", "instance_public_ips"],
-    cwd=terraform_dir
+    ["terraform", "output", "-json", "instance_public_ips"], cwd=terraform_dir
 )
 
 # terraform_output = subprocess.check_output(["terraform", "output", "-json", "instance_public_ips"])
@@ -19,30 +20,30 @@ controller_ip = ips[0]
 worker_ips = ips[1:]
 
 inventory = {
-    'all': {
-            'master': {
-                'hosts': {
-                    f'master1': {
-                        'ansible_host': controller_ip,
-                        'ansible_user': 'root',
-                        'ansible_port': 22,
+    "all": {
+        "children": {
+            "master": {
+                "hosts": {
+                    f"master1": {
+                        "ansible_host": controller_ip,
+                        "ansible_user": "root",
+                        "ansible_port": 22,
                     }
                 }
             },
-            'worker': {
-                'hosts': {}
-            }
+            "worker": {"hosts": {}},
+        }
     }
 }
 
 for i, ip in enumerate(worker_ips, 1):
-    inventory['all']['worker']['hosts'][f'worker{i}'] = {
-        'ansible_host': ip,
-        'ansible_user': 'root',
-        'ansible_port': 22,
+    inventory["all"]["children"]["worker"]["hosts"][f"worker{i}"] = {
+        "ansible_host": ip,
+        "ansible_user": "root",
+        "ansible_port": 22,
     }
 
-with open('ansible/inventory/hosts.yml', 'w') as file:
+with open("ansible/inventory/hosts.yml", "w") as file:
     yaml.dump(inventory, file, default_flow_style=False, indent=2)
 
 print("Inventory file 'hosts.yml' generated successfully!")
