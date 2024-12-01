f9d649c349985d3924ea92c1d16878616a2acc53
AFz <0xafz@proton.me>
2024-12-02 00:04:06 +0330

    refactor: change domain var to lowercase

---
+++ prod/terraform/storm/variables.tf
@@ -0,0 +1,34 @@
+variable "CI_REGISTRY_USER" {
+  description = "gitlab registry username"
+  type        = string
+}
+
+variable "CI_REGISTRY_PASSWORD" {
+  description = "gitlab registry access token"
+  type        = string
+}
+
+variable "CI_REGISTRY_EMAIL" {
+  description = "gitlab registry email"
+  type        = string
+}
+
+variable "domain" {
+  description = "sub domain of project"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "project image tag"
+  type        = string
+}
+
+variable "grpc_server_addr" {
+  description = "gRPC server addr"
+  type        = string
+}
+
+variable "kafka_broker_list" {
+  description = "kafka cluster broker list"
+  type        = string
+}
